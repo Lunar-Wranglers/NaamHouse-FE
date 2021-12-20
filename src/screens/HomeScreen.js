50c6ae85b3e5f63a40b24c5de200647239c26a2c
Matt Moore <Matt.M.Moore13@outlook.com>
2021-12-20 12:27:10 -0500

    - Began carousel work - swtiching branches to attempt a ViewPropTypes error

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,28 @@ import { useAsyncStorage } from '@react-native-async-storage/async-storage'
 import React, { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { Card } from 'react-native-elements'
+import Carousel from 'react-native-snap-carousel'
 
 export default function HomeScreen ({ navigation }) {
+  
+  _renderItem = ({item, index}) => {
+    return (
+      <View style={styles.slide}>
+        <Text style={styles.title}>
+          { item.title }
+        </Text>
+      </View>
+    )
+  }
+  
   return(
-    <View>
-      Howdy
-    </View>
+    <Carousel
+      ref={(c) => { this._carousel = c; }}
+      data={this.state.entries}
+      renderItem={this._renderItem}
+      sliderWidth={sliderWidth}
+      itemWidth={itemWidth}
+    />
   )
 }
 
